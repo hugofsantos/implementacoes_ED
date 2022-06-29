43440450d766421403722d51f95b4422907c3fa0
Hugo Santos <hf.santos2332@gmail.com>
2022-06-28 21:08:28 -0300

    Implementa sequência com vetor

--- include/sequencias.h
+++ include/sequencias.h
@@ -4,30 +4,40 @@
 /**
  * @brief TAD sequência implementada como vetor
  * 
- * @param tamanho Tamanho da sequência
+ * @param tamanho Tamanho máximo da sequência
+ * @param n Quantidade de elementos que estão inseridos na sequência
  * @param valores Vetor de valores armazenados na sequência
  */
 typedef struct{
     unsigned int tamanho;
+    unsigned int n;
     int *valores;
 } SequenciaVetor;
 
 /**
- * @brief 
+ * @brief Procura o valor de uma determinada posição em uma sequência
  * 
- * @param sequencia Sequência em que você irá buscar o valor
+ * @param sequencia referência para a sequência em que você irá buscar o valor
  * @param posicao Posição do valor que você quer acessar
  * @return int Valor buscado (ou 0 se a posição não for válida) 
  */
-int acessarValorSeqV(SequenciaVetor sequencia, unsigned int posicao);
+int acessarValorSeqV(SequenciaVetor *sequencia, unsigned int posicao);
 
 /**
  * @brief Insere um valor em uma sequência implementada com vetor
  * 
- * @param sequencia Sequência na qual o elemento será inserido
+ * @param sequencia referência para a sequência na qual o elemento será inserido
  * @param valor Valor a ser inserido
  * @param posicao Posição onde o valor deve ser inserido
  */
-void inserirValorSeqV(SequenciaVetor sequencia, int valor, unsigned int posicao);
+void inserirValorSeqV(SequenciaVetor *sequencia, int valor, int posicao);
+
+/**
+ * @brief Remove um valor de uma determinada posição em uma sequência implementada com vetor
+ * 
+ * @param sequencia referência para a sequência na qual o elemento será removido
+ * @param posicao Posição onde o valor deve ser removido
+ */
+void removerValorSeqV(SequenciaVetor *sequencia, unsigned int posicao);
 
 #endif
--- src/sequencias.c
+++ src/sequencias.c
@@ -1,12 +1,34 @@
 #include "sequencias.h"
 
-int acessarValorSeqV(SequenciaVetor sequencia, unsigned int posicao){
-    if(posicao < 0 || posicao >= sequencia.tamanho) return 0;
+int acessarValorSeqV(SequenciaVetor *sequencia, unsigned int posicao){
+    if(posicao < 0 || posicao >= sequencia->tamanho) return 0;
 
-    return sequencia.valores[posicao];
+    return sequencia->valores[posicao];
 }
 
-void inserirValorSeqV(SequenciaVetor sequencia, int valor, unsigned int posicao){
-    if(posicao < 0 || posicao >= sequencia.tamanho) return;
-    
+void inserirValorSeqV(SequenciaVetor *sequencia, int valor, int posicao){
+    if(posicao < 0 || posicao >= sequencia->tamanho || sequencia->n == sequencia->tamanho) return;
+
+    if(posicao >= sequencia->n){
+        sequencia->valores[sequencia->n++] = valor;
+        return;
+    }
+
+    for(int i = sequencia->n - 1; i >= posicao; i--){
+        sequencia->valores[i+1] = sequencia->valores[i];
+    }
+
+    sequencia->valores[posicao] = valor;
+    sequencia->n++;
+}
+
+void removerValorSeqV(SequenciaVetor *sequencia, unsigned int posicao){
+    if(posicao < 0 || posicao >= sequencia-> n || sequencia->n == 0) return;
+
+    for(unsigned int i = posicao; i < sequencia->n - 1; i++){
+        sequencia->valores[i] = sequencia->valores[i+1];
+    }
+
+    sequencia->valores[sequencia->n - 1] = 0;
+    sequencia->n--;
 }
